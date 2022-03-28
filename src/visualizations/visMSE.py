eadc109cf83a93972b6739becf72dc8a6ff263e6
cornelis <cornelis.zwart.cz@gmail.com>
2022-03-28 11:03:45 +0200

    visMSE some fixes for my models

--- src/visualizations/visMSE.py
+++ src/visualizations/visMSE.py
@@ -1,21 +1,21 @@
 import os, sys
-from predictor import gen_prediction, write_file
+from src.predictor import gen_prediction, write_file
 import pickle as pkl
 import pandas as pd
-from modelTraining.model_factory import mse
+from src.modelTraining.model_factory import mse
 import numpy as np
 
 
 from keras.models import load_model
 
 
-prerequesites = pkl.load(open("../data/model_prereq.pkl", 'rb'))
-Lindel_training = pd.read_csv("../data/Lindel_test.txt", sep='\t', header=None)
+prerequesites = pkl.load(open("C:\\Users\\corne\\PycharmProjects\\mlbio\\data\\model_prereq.pkl", 'rb'))
+Lindel_training = pd.read_csv("C:\\Users\\corne\\PycharmProjects\\mlbio\\data\\Lindel_test.txt", sep='\t', header=None)
 # column descriptions
 gseq = Lindel_training.iloc[:, 0]  # guide sequences
 feat = Lindel_training.iloc[:, 1:3034]  # 3033 binary features [2649 MH binary features + 384 one hot encoded features]
 hencode = feat.iloc[:, -384:] # One hot encoded features
-ins = hencode.iloc[:, -104:] # Insertion features
+ins = np.load("C:\\Users\\corne\\PycharmProjects\\mlbio\\data\\insTriData104.txt.npy") # Insertion features
 obsp = Lindel_training.iloc[:,3034:]  # 557 observed outcome frequencies
 
 msearray = []
@@ -28,12 +28,12 @@ frame_shift = prerequesites[3]
 for i in range(0, 440):
     mode = "l2"
     if mode == "l2":
-        indel, deletion, insertion = load_model("../models/indel_l2.h5"), load_model("../models/deletion_l2.h5"), load_model("../models/insertion_l2.h5")
+        indel, deletion, insertion = load_model("C:\\Users\\corne\\PycharmProjects\\mlbio\\models\\indel_l2.h5"), load_model("C:\\Users\\corne\\PycharmProjects\\mlbio\\models\\deletion_l2.h5"), load_model("C:\\Users\\corne\\PycharmProjects\\mlbio\\models\\insertion_l2.h5")
     else:
         indel, deletion, insertion = load_model("../models/indel_l1.h5"), load_model("../models/deletion_l1.h5"), load_model("../models/insertion_l1.h5")
 
     # Generate the predicted value and frameshift
-    y_hat, fs = gen_prediction(hencode.iloc[i, :].to_numpy(), ins.iloc[i, :].to_numpy(), feat.iloc[i, :].to_numpy(),
+    y_hat, fs = gen_prediction(hencode.iloc[i, :].to_numpy(), ins, feat.iloc[i, :].to_numpy(),
                                prerequesites, indel, deletion, insertion)
     # Get the observed value
     y_obs = obsp.iloc[i, :].to_numpy()
@@ -76,8 +76,8 @@ file.write(str(msearray))
 file.close()
 
 file2 = open("testFS.txt", 'w')
-fil2.write(str(fsarray))
-fil2.close()
+file2.write(str(fsarray))
+file2.close()
 
 file3 = open("testBox.txt", 'w')
 file3.write(str(boxarray))
